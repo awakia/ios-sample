0d322829242dcc2e9a5648e9903c3d233683efe2
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-04-16 20:54:09 +0900

    Add Xcode generated codes

---
+++ IosSample/IosSampleTests/IosSampleTests.h
@@ -0,0 +1,13 @@
+//
+//  IosSampleTests.h
+//  IosSampleTests
+//
+//  Created by Naoyoshi Aikawa on 4/16/13.
+//  Copyright (c) 2013 Naoyoshi Aikawa. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IosSampleTests : SenTestCase
+
+@end
---
+++ IosSample/IosSampleTests/IosSampleTests.m
@@ -0,0 +1,32 @@
+//
+//  IosSampleTests.m
+//  IosSampleTests
+//
+//  Created by Naoyoshi Aikawa on 4/16/13.
+//  Copyright (c) 2013 Naoyoshi Aikawa. All rights reserved.
+//
+
+#import "IosSampleTests.h"
+
+@implementation IosSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in IosSampleTests");
+}
+
+@end
